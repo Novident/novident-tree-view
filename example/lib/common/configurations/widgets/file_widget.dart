89170c4f374ef5e7ba02f74c22c5b683d61e776f
Cathood <santiagowmar@gmail.com>
2025-04-22 21:13:00 -0400

    Chore: wrapped file widget into listenable builder

--- example/lib/common/configurations/widgets/file_widget.dart
+++ example/lib/common/configurations/widgets/file_widget.dart
@@ -1,12 +1,7 @@
-import 'package:example/common/configurations/widgets/trailing_menu/trailing_menu_widget.dart';
 import 'package:example/common/controller/tree_controller.dart';
 import 'package:example/common/nodes/file.dart';
-import 'package:example/extensions/node_ext.dart';
-import 'package:example/extensions/num_ext.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_quill/internal.dart';
-import 'package:novident_nodes/novident_nodes.dart';
 
 class FileTile extends StatefulWidget {
   final File file;
@@ -27,30 +22,31 @@ class _FileTileState extends State<FileTile> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3),
       child: ListenableBuilder(
-          listenable: widget.file,
-          builder: (context, child) {
-            return Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 5),
-                  child: Icon(
-                    widget.file.content.isEmpty
-                        ? CupertinoIcons.doc_text
-                        : CupertinoIcons.doc_text_fill,
-                    size: isAndroid ? 20 : null,
-                  ),
+        listenable: widget.file,
+        builder: (context, child) {
+          return Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: Icon(
+                  widget.file.content.isEmpty
+                      ? CupertinoIcons.doc_text
+                      : CupertinoIcons.doc_text_fill,
+                  size: isAndroid ? 20 : null,
                 ),
-                Expanded(
-                  child: Text(
-                    "${widget.file.name} ${widget.file.level}",
-                    maxLines: 1,
-                    softWrap: true,
-                    overflow: TextOverflow.fade,
-                  ),
+              ),
+              Expanded(
+                child: Text(
+                  "${widget.file.name} ${widget.file.level}",
+                  maxLines: 1,
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
                 ),
-              ],
-            );
-          }),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
